a66dc17a5d0e05ed848125b2fe802fe64e817a7c
JolidzeHome <energyy@inbox.ru>
2022-09-10 21:01:33 +0300

    Initial commit

---
+++ Example008_IntroFunction/Program.cs
@@ -0,0 +1,10 @@
+﻿//напишем функцию
+int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1 1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+
---
+++ HomeWork_Task19/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//           является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+int palin = Convert.ToInt32(Console.ReadLine()); 
+
+while (palin < 10000 || palin > 99999)
+{
+    Console.Write("Введите пятизначное число: ");
+    palin = Convert.ToInt32(Console.ReadLine()); 
+}
+
+int x1 = palin / 10000; //первая цифра числа
+int x2 = palin / 1000 % 10; //вторая
+int x4 = palin % 100 / 10; //четвертая
+int x5 = palin % 10; //пятая
+
+Console.WriteLine("x1 = " + x1 + ", x2 = " + x2 + ", x4 = " + x4 + ", x5 = " + x5);
